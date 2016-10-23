3f33813f953b8b2bbe2741caba59cf61c1a68e05
Nick Benson <nickjbenson@gmail.com>
2016-10-22 22:43:05 -0700

    Initial commit! Build currently OSX only.

---
+++ rtmidi/CMakeFiles/RTMidiCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTMidiCMake.dir/RtMidi.cpp.o"
+  "libRTMidiCMake.pdb"
+  "libRTMidiCMake.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RTMidiCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rtmidi/CMakeFiles/rtmidi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtmidi.dir/RtMidi.cpp.o"
+  "librtmidi.pdb"
+  "librtmidi.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtmidi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
